11e5d0c11934cef40e643dfce77d16a541554ef6
TheBackpacker1 <mohamedlouati002@gmail.com>
2024-01-18 22:51:51 +0100

    first commit Weather Application

---
+++ src/screens/UpComingWeather.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, FlatList,View,StatusBar,ImageBackground } from'react-native'
+import ListItem from '../components/ListItem'
+
+const DATA = [
+  {
+    dt_txt: "2023-11-18 12:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'clear'
+      }
+    ]
+  },
+  {
+    dt_txt: "2023-11-18 15:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Clouds'
+      }
+    ]
+  },
+  {
+    dt_txt: "2023-11-18 18:00:00",
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55
+    },
+    weather: [
+      {
+        main: 'Rain'
+      }
+    ]
+  },
+]
+const Item =(props)=> {
+   const{dt_txt,min,max,condition}=props
+   return (
+<View style={styles.item}> 
+  <Feather name={'sun'} size={50} color={'white'}/> 
+  <Text style={styles.date}>{dt_txt} </Text>
+  <Text style={styles.temp}> {min}</Text>
+  <Text style={styles.temp}>{max} </Text>
+</View>
+
+   )
+
+}
+const UpComingWeather = () => {
+  const renderItem=({item})=> (
+
+    <ListItem 
+     condition={item.weather[0].main}
+     dt_txt={item.dt_txt}
+      min={item.main.temp_min}  
+      max={item.main.temp_max}
+    />
+  )
+  const { container,image}=styles
+  return (
+    <SafeAreaView style={styles.container}>
+          <ImageBackground
+           source={require('../../assets/upcoming-back.jpg')}
+            style={image}>
+        <Text> UpComingWeather </Text>
+      <FlatList data={ DATA} renderItem={renderItem} keyExtractor={(item)=> item.dt_txt} />
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop:StatusBar.currentHeight ||0,
+    backgroundColor:'royalblue'
+  },
+  
+  image:{
+   flex:1
+  }
+})
+export default UpComingWeather
